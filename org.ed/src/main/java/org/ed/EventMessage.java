cad2324e7f3983f1e5be5c4a6d0575cb019afaca
tao <cuitaocrazy@163.com>
2015-01-20 16:19:00 +0800

    加入事件消息产生时间

--- org.ed/src/main/java/org/ed/EventMessage.java
+++ org.ed/src/main/java/org/ed/EventMessage.java
@@ -5,13 +5,9 @@ import java.util.Date;
 
 public class EventMessage implements Serializable {
 	private static final long serialVersionUID = -7424071454218566287L;
-	private Date MessageDate;
+	private Date messageDate = new Date();
 
 	public Date getMessageDate() {
-		return MessageDate;
-	}
-
-	public void setMessageDate(Date messageDate) {
-		MessageDate = messageDate;
+		return messageDate;
 	}
 }
